799834ed94da0235ea9f270c0e182333153549b0
suryaguntursuprapto <suryaguntur2000@gmail.com>
2024-12-20 20:59:45 +0700

    membuat RepositoryRS

---
+++ app/src/main/java/com/example/ucp2_20220140002/repository/LocalRepositoryRS.kt
@@ -0,0 +1,7 @@
+package com.example.ucp2_20220140002.repository
+
+
+class LocalRepositoryRS(
+){
+
+}
---
+++ app/src/main/java/com/example/ucp2_20220140002/repository/RepositoryRS.kt
@@ -0,0 +1,23 @@
+package com.example.ucp2_20220140002.repository
+
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.ucp2_20220140002.data.entity.Dokter
+import com.example.ucp2_20220140002.data.entity.Jadwal
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoryRS {
+    //tabel atau entitas dokter
+    suspend fun insertDokter(dokter: Dokter)
+    fun getAllDokter(): Flow<List<Dokter>>
+    fun getDokter(id: String): Flow<Dokter>
+
+    //dari entitas jadwal
+    suspend fun insertJadwal(jadwal: Jadwal)
+    fun getAllJadwal() :Flow<List<Jadwal>>
+    fun getJadwal(id: String) : Flow<Jadwal>
+    suspend fun deleteJadwal(jadwal: Jadwal)
+    suspend fun updateJadwal(jadwal: Jadwal)
+}
