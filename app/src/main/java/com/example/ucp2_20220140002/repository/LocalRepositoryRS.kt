4c0ca4e2332d46aa63db6c8c597f5a1b7d7e0340
suryaguntursuprapto <suryaguntur2000@gmail.com>
2024-12-20 21:00:18 +0700

    membuat LocalRepositoryRS

--- app/src/main/java/com/example/ucp2_20220140002/repository/LocalRepositoryRS.kt
+++ app/src/main/java/com/example/ucp2_20220140002/repository/LocalRepositoryRS.kt
@@ -1,7 +1,40 @@
 package com.example.ucp2_20220140002.repository
 
+import com.example.ucp2_20220140002.data.dao.DokterDAO
+import com.example.ucp2_20220140002.data.dao.JadwalDAO
+import com.example.ucp2_20220140002.data.entity.Dokter
+import com.example.ucp2_20220140002.data.entity.Jadwal
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryRS(
-){
+    private val dokterDAO: DokterDAO,
+    private val jadwalDAO: JadwalDAO
+): RepositoryRS {
+    // dari entitas dokter
+   override suspend fun insertDokter(dokter: Dokter){
+       dokterDAO.insertDokter(dokter)
+   }
+    override fun getAllDokter(): Flow<List<Dokter>> {
+       return dokterDAO.getAllDokter()
+    }
+    override fun getDokter(id: String): Flow<Dokter> {
+        return dokterDAO.getDokter(id = id)
+    }
 
+    //dari entitas jadwal
+    override suspend fun insertJadwal(jadwal: Jadwal) {
+        jadwalDAO.insertJadwal(jadwal)
+    }
+    override fun getAllJadwal(): Flow<List<Jadwal>> {
+        return jadwalDAO.getAllJadwal()
+    }
+    override fun getJadwal(id: String): Flow<Jadwal> {
+        return jadwalDAO.getJadwal(id = id)
+    }
+    override suspend fun deleteJadwal(jadwal: Jadwal) {
+        jadwalDAO.deleteJadwal(jadwal)
+    }
+    override suspend fun updateJadwal(jadwal: Jadwal) {
+        jadwalDAO.updateJadwal(jadwal)
+    }
 }
